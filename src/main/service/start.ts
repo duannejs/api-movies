0e5a22cd99ae5ccce855ef46f4c4cd57b17fb48d
Duanne Schaedler <duannejs@gmail.com>
2021-09-28 22:48:40 -0300

    feat: start csv file

---
+++ src/main/service/start.ts
@@ -0,0 +1,11 @@
+const neatCsv = require('neat-csv');
+
+const fs = require('fs')
+
+fs.readFile('./file.csv', async (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(await neatCsv(data))
+})
